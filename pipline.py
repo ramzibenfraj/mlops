af9030d55e944a695330327e39ba31ed30ff675f
ramzibf <rbenfraj111@gmail.com>
2024-12-18 17:06:43 +0000

    fix bug

--- pipline.py
+++ pipline.py
@@ -1,3 +1,4 @@
+import os
 import logging
 import yaml
 import mlflow
@@ -9,16 +10,19 @@ from steps.predict import Predictor
 from sklearn.metrics import classification_report
 
 # Set up logging
-logging.basicConfig(level=logging.INFO,format='%(asctime)s:%(levelname)s:%(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
 
 
 def train_with_mlflow():
+    # Set MLflow tracking directory
+    tracking_uri = './mlruns'  # Save runs locally in the repository
+    os.makedirs(tracking_uri, exist_ok=True)  # Ensure the directory exists
+    os.environ['MLFLOW_TRACKING_URI'] = tracking_uri
+    mlflow.set_experiment("Model Training Experiment")
 
     with open('config.yml', 'r') as file:
         config = yaml.safe_load(file)
 
-    mlflow.set_experiment("Model Training Experiment")
-    
     with mlflow.start_run() as run:
         # Load data
         ingestion = Ingestion()
@@ -37,27 +41,29 @@ def train_with_mlflow():
         trainer.train_model(X_train, y_train)
         trainer.save_model()
         logging.info("Model training completed successfully")
-        
-        # Evaluate model 
+
+        # Evaluate model
         predictor = Predictor()
         X_test, y_test = predictor.feature_target_separator(test_data)
         accuracy, class_report, roc_auc_score = predictor.evaluate_model(X_test, y_test)
         report = classification_report(y_test, trainer.pipeline.predict(X_test), output_dict=True)
         logging.info("Model evaluation completed successfully")
-        
-        # Tags 
+
+        # Tags
         mlflow.set_tag('Model developer', 'prsdm')
         mlflow.set_tag('preprocessing', 'OneHotEncoder, Standard Scaler, and MinMax Scaler')
-        
-        # Log metrics
+
+        # Log metrics and parameters
         model_params = config['model']['params']
         mlflow.log_params(model_params)
         mlflow.log_metric("accuracy", accuracy)
         mlflow.log_metric("roc", roc_auc_score)
+
+        # Log the model
         mlflow.sklearn.log_model(trainer.pipeline, "model")
-                
+
         # Register the model
-        model_name = "insurance_model" 
+        model_name = "insurance_model"
         model_uri = f"runs:/{run.info.run_id}/model"
         mlflow.register_model(model_uri, model_name)
 
@@ -69,7 +75,7 @@ def train_with_mlflow():
         print(f"Accuracy Score: {accuracy:.4f}, ROC AUC Score: {roc_auc_score:.4f}")
         print(f"\n{class_report}")
         print("=====================================================\n")
-        
+
+
 if __name__ == "__main__":
-    # main()
     train_with_mlflow()
